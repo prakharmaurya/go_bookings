1cbe21d1cb45fcdce77ecbca428e5cc8afa2d58f
Prakhar Maurya <000.excel@gmail.com>
2021-10-05 22:44:23 +0530

    some test page template added

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,8 +18,11 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(NoSurf)
 	mux.Use(app.Session.LoadAndSave)
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -27,7 +27,7 @@ func NewHandlers(r *Repository) {
 func (m *Repository) Home(rw http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.app.Session.Put(r.Context(), "remote_ip", remoteIP)
-	render.RenderTemplate(rw, "home.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(rw, "home.page.tmpl", &models.TemplateData{IsAuthenticated: 0})
 }
 
 func (m *Repository) About(rw http.ResponseWriter, r *http.Request) {
@@ -37,4 +37,14 @@ func (m *Repository) About(rw http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(rw, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
+}
+
+func (m *Repository) Reservation(rw http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(rw, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) Majors(rw http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(rw, "majors.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) Generals(rw http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(rw, "generals.page.tmpl", &models.TemplateData{})
 }
